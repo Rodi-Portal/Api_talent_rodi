1e588857d07d21e13edad5adce871ef1237083b8
DevRodi <rodi.control@gmail.com>
2024-06-21 08:33:41 -0600

    probando   test controller x2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Response;
 
 class TestController extends Controller
 {
