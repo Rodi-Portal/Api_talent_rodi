70a605a9bd85d548deeaad623f257ace21800df0
DevRodi <rodi.control@gmail.com>
2024-07-04 10:15:45 -0600

    probando para  cargar  archivos en talentSafeControl

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class DocumentController extends Controller
 {
@@ -19,15 +21,9 @@ class DocumentController extends Controller
         $file = $request->file('file');
         $fileName = $request->input('file_name');
         $carpeta = $request->input('carpeta');
-         if($carpeta != null){
-  // Preparar la ruta de destino
-  $destinationPath = 'https://portal.talentsafecontrol.com/_psicometria/';
-         }else{
-              // Preparar la ruta de destino
-        $destinationPath = 'https://portal.talentsafecontrol.com/_docs/';
-         }
 
-      
+        // Preparar la ruta de destino
+        $destinationPath = $carpeta ? 'https://portal.talentsafecontrol.com/_psicometria/' : 'https://portal.talentsafecontrol.com/_docs/';
 
         // Loggear los valores antes de mover el archivo
         Log::info('Moviendo archivo:', [
@@ -35,9 +31,16 @@ class DocumentController extends Controller
             'destination_path' => $destinationPath,
         ]);
 
-        // Mover el archivo a la ruta de destino
+        // Guardar temporalmente el archivo en el servidor local
+        $tempPath = storage_path('app/temp/' . $fileName);
+        $file->move(storage_path('app/temp'), $fileName);
+
+        // Enviar el archivo al servidor remoto
         try {
-            $file->move($destinationPath, $fileName);
+            $this->sendFileToRemoteServer($tempPath, $fileName, $destinationPath);
+
+            // Eliminar el archivo temporal
+            File::delete($tempPath);
 
             return response()->json([
                 'status' => 'success',
@@ -49,10 +52,46 @@ class DocumentController extends Controller
                 'exception' => $e->getMessage()
             ]);
 
+            // Eliminar el archivo temporal en caso de error
+            File::delete($tempPath);
+
             return response()->json([
                 'status' => 'error',
                 'message' => 'Error al mover el archivo: ' . $e->getMessage(),
             ], 500);
+        }
+    }
+
+    private function sendFileToRemoteServer($filePath, $fileName, $destinationUrl)
+    {
+        $ch = curl_init();
+
+        $cfile = new \CURLFile($filePath, mime_content_type($filePath), $fileName);
+
+        $data = array(
+            'file' => $cfile,
+            'file_name' => $fileName
+        );
+
+        curl_setopt($ch, CURLOPT_URL, $destinationUrl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            $error_msg = curl_error($ch);
+            curl_close($ch);
+            throw new \Exception('Error al enviar el archivo: ' . $error_msg);
+        }
+
+        curl_close($ch);
+
+        $responseData = json_decode($response, true);
+
+        if ($responseData['status'] != 'success') {
+            throw new \Exception('Error al mover el archivo: ' . $responseData['message']);
         }
     }
 }
