667c3f74abc62d4fce0b3ee15957b8f2ceb847bd
DevRodi <rodi.control@gmail.com>
2024-11-21 23:44:55 -0600

    Subiendo  descarga  de  reporte de Excel x11

--- config/cors.php
+++ config/cors.php
@@ -1,30 +1,12 @@
 <?php
 
 return [
-    /*
-     |--------------------------------------------------------------------------
-     | Laravel CORS Options
-     |--------------------------------------------------------------------------
-     |
-     | The allowed_origins and allowed_origins_patterns options allow you to
-     | configure what domains (or patterns) can send requests to your
-     | application. Allowed_methods and allowed_headers are self-explanatory.
-     |
-     */
-
-    'paths' => ['api/*'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['https://portal.talentsafecontrol.com', '*'],
-
-    'allowed_origins_patterns' => ['*'],
-
-    'allowed_headers' => ['*'],
-
+    'paths' => [],
+    'allowed_methods' => [],
+    'allowed_origins' => [],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => [],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
