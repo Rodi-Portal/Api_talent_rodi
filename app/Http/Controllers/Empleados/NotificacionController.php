9918336960949876d6340606af427d7490b85162
DevRodi <rodi.control@gmail.com>
2025-04-12 18:37:51 -0600

    subiendo cambios   para  notificaciones  exempleados

--- app/Http/Controllers/Empleados/NotificacionController.php
+++ app/Http/Controllers/Empleados/NotificacionController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller; // Asegúrate de agregar esto
 
 
 use App\Models\Notificacion;
+use App\Models\NotificacionExempleo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
@@ -25,6 +26,28 @@ class NotificacionController extends Controller
         }elseif($status == 1)
         {
             $notificacion = Notificacion::where('id_portal', $id_portal)
+            ->where('id_cliente', $id_cliente)
+            ->where('status', $status)
+            ->first();
+        }
+        // Si la notificación no existe, devolver un mensaje indicando que no hay datos
+        if (!$notificacion) {
+            return response()->json(['message' => 'No se encontraron notificaciones para este cliente y portal.'], 404);
+        }
+    
+        // Si la notificación existe, devolverla
+        return response()->json(['notificacion' => $notificacion], 200);
+    }
+    public function consultarExempleo($id_portal, $id_cliente, $status)
+    {
+        if($status == 2){ 
+        $notificacion = NotificacionExempleo::where('id_portal', $id_portal)
+            ->where('id_cliente', $id_cliente)
+            ->where('notificacionesActivas', $status)
+            ->first();
+        }elseif($status == 1)
+        {
+            $notificacion = NotificacionExempleo::where('id_portal', $id_portal)
             ->where('id_cliente', $id_cliente)
             ->where('status', $status)
             ->first();
@@ -82,6 +105,49 @@ class NotificacionController extends Controller
 
         // Si no existe, crear uno nuevo
         $notificacion = Notificacion::create($request->all());
+        return response()->json(['message' => 'Notificación creada correctamente.', 'notificacion' => $notificacion], 201);
+    }
+
+
+    public function guardarExempleados(Request $request)
+    {
+
+         // Verifica si los datos están llegando
+         Log::debug('Datos recibidos en guardar:', $request->all());        // Validar que los parámetros necesarios estén presentes
+        $validator = Validator::make($request->all(), [
+            'id_portal' => 'required|integer', // Aseguramos que id_portal existe
+            'id_cliente' => 'required|integer', // Aseguramos que id_cliente existe
+            'correo' => 'nullable|boolean',
+            'correo1' => 'nullable|string',
+            'correo2' => 'nullable|string',
+            'exempleo' => 'nullable|boolean',
+            'horarios' => 'nullable|string',
+            'ladaSeleccionada' => 'nullable|string',
+            'ladaSeleccionada2' => 'nullable|string',
+            'notificacionesActivas' => 'nullable|integer',
+            'status' => 'nullable|boolean',
+            'telefono1' => 'nullable|string',
+            'telefono2' => 'nullable|string',
+            'whatsapp' => 'nullable|boolean',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        // Buscar si ya existe un registro con el mismo id_portal y id_cliente
+        $notificacion = NotificacionExempleo::where('id_portal', $request->id_portal)
+            ->where('id_cliente', $request->id_cliente)
+            ->first();
+
+        // Si existe, actualizar los datos
+        if ($notificacion) {
+            $notificacion->update($request->all());
+            return response()->json(['message' => 'Notificación actualizada correctamente.', 'notificacion' => $notificacion], 200);
+        }
+
+        // Si no existe, crear uno nuevo
+        $notificacion = NotificacionExempleo::create($request->all());
         return response()->json(['message' => 'Notificación creada correctamente.', 'notificacion' => $notificacion], 201);
     }
 }
---
+++ app/Models/NotificacionExempleo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;  // Asegúrate de importar el trait
+use Illuminate\Database\Eloquent\Model;
+class NotificacionExempleo extends Model
+{
+    use HasFactory;
+
+    // Si no estás utilizando las migraciones, se debe indicar la conexión y la tabla de la base de datos.
+    protected $connection = 'portal_main'; // Asegúrate de que sea la conexión correcta
+    protected $table = 'notificaciones_exempleados'; // El nombre de la tabla en la base de datos
+
+    // Indicamos que no se manejen timestamps
+    public $timestamps = false;
+
+    // Definir los campos que son asignables en masa
+    protected $fillable = [
+        'creacion',
+        'correo',
+        'correo1',
+        'correo2',
+        'exempleo',
+        'horarios',
+        'id_cliente',
+        'id_portal',
+        'id_usuario',
+        'ladaSeleccionada',
+        'ladaSeleccionada2',
+        'notificacionesActivas',
+        'status',
+        'telefono1',
+        'telefono2',
+        'whatsapp',
+    ];
+
+    // Relación con la tabla 'cliente'
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class, 'id_cliente');
+    }
+
+    // Relación con la tabla 'portal'
+    public function portal()
+    {
+        return $this->belongsTo(Portal::class, 'id_portal');
+    }
+
+    // Relación con la tabla 'usuarios_portal'
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'id_usuario');
+    }
+}
