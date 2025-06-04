b40603eaa428506c929ec8350817cc69102d4cd0
DevRodi <rodi.control@gmail.com>
2025-06-04 09:50:57 -0600

    mostrar  estado de los documentos

--- app/Http/Controllers/Empleados/EmpleadoController.php
+++ app/Http/Controllers/Empleados/EmpleadoController.php
@@ -87,7 +87,7 @@ class EmpleadoController extends Controller
             foreach ($empleados as $empleado) {
                 // Obtener documentos del empleado
                 $documentos = DocumentEmpleado::where('employee_id', $empleado->id)
-                    ->where('status', 1)
+                   
                               ->get();
                 $cursos     = CursoEmpleado::where('employee_id', $empleado->id)->get();
                 $examenes   = ExamEmpleado::where('employee_id', $empleado->id)->get();
