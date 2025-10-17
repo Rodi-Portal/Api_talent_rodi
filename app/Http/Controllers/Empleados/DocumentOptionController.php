76abceb45abbc18164989144fdf72dd13584e893
DevRodi <rodi.control@gmail.com>
2025-10-17 12:05:27 -0600

    subiendo archivos  grandes x3

--- app/Http/Controllers/Empleados/DocumentOptionController.php
+++ app/Http/Controllers/Empleados/DocumentOptionController.php
@@ -712,7 +712,7 @@ class DocumentOptionController extends Controller
             'description'     => 'nullable|string|max:500',
             'expiry_date'     => 'nullable|date',
             'expiry_reminder' => 'nullable|integer',
-            'file'            => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:2048',
+            'file'            => 'nullable|file|mimes:jpg,jpeg,png,pdf|max:10240',
             'id_portal'       => 'required|integer',
             'carpeta'         => 'nullable|string|max:255',
 
