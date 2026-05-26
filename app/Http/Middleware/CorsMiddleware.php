302ba9fd17de2ce948be4ad77a861ed1cb131b67
DevRodi <rodi.control@gmail.com>
2026-05-26 10:10:57 -0600

    cambios  para    soft delete

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -12,13 +12,13 @@ class CorsMiddleware
         $origin = $request->headers->get('Origin');
 
         $allowedOrigins = [
-            'https://sandbox.talentsafecontrol.com',
-            'https://dev.rodi.com.mx',
-            'https://sandboxmiportal.talentsafecontrol.com',
-            'http://localhost',
-            'http://localhost:8080',
-            'http://localhost:8000',
-            'http://localhost:5173',
+            'https://portal.talentsafecontrol.com',
+            'https://rodicontrol.rodi.com.mx',
+            'https://miportal.talentsafecontrol.com',
+            //'http://localhost',
+            //'http://localhost:8000',
+            //'http://localhost:5173',
+            //'http://localhost:5174',
         ];
 
         /*
