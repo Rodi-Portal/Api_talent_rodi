6ae94679f55f3817fcde7fd42c3f180a40f6b7ea
DevRodi <rodi.control@gmail.com>
2026-09-11 16:51:46 -0600

    Permitir portal local AWS en CORS

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,6 +17,7 @@ class CorsMiddleware
             'http://localhost',
             'http://localhost:8000',
             'http://127.0.0.1:8000',
+            'http://127.0.0.1:8081',
             'http://localhost:5173',
             'http://localhost:5174',
        
