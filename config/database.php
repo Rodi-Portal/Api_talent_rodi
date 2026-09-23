344735e4392f0f2dddf4f80f030f0096d009bb15
DevRodi <rodi.control@gmail.com>
2026-09-23 13:36:05 -0600

    Retirar conexion directa a RODI en AWS

--- config/database.php
+++ config/database.php
@@ -42,24 +42,6 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-        'rodi_main' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => env('DB_SSL_CA') ? [
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('DB_SSL_VERIFY_SERVER_CERT', true),
-            ] : [],
-        ],
 
         'portal_main' => [
             'driver' => 'mysql',
